5765dd2276bdc447195a1bc0621190a3e3f2d054
Christopher Gutteridge <cjg@lemur5.ecs.soton.ac.uk>
2014-05-09 16:30:30 +0100

    remove some obvious utility urls

--- plugins.d/twitter.php
+++ plugins.d/twitter.php
@@ -3,7 +3,16 @@ class CensusPluginTwitterAccounts extends CensusPluginRegexpList
 {
 	protected $id = "twitterAccounts";	
 	protected $caseSensitive = false;
-	protected $regexp = "[='\"]https?:\/\/(www\.)?twitter.com\/([^ >'\"]+)";
-	public function addMatch( $matches ) { return $matches[2]; }
+	protected $regexp = "[='\"]https?:\/\/(www\.)?twitter.com\/(#!\/)?([^ \/>'\"\?]+)";
+	public function addMatch( $matches ) 
+	{ 
+		// no easy way to spot twitter's utility URLs
+		if( $matches[3]=="search" ) { return false; }
+		if( $matches[3]=="intent" ) { return false; }
+		if( $matches[3]=="share" ) { return false; }
+		if( $matches[3]=="statuses" ) { return false; }
+		if( $matches[3]=="home" ) { return false; }
+		return $matches[3]; 
+	}
 }
 CensusPluginRegister::instance()->register( "CensusPluginTwitterAccounts" );
